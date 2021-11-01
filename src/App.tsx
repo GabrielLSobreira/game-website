eeef748f4961a65212f7f9d76a6844b0dbf731be
Gabriel Sobreira <gabriellaranjasobreira@gmail.com>
2021-11-01 08:01:38 -0300

    Criação do componente Home e rota

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-const App = () => {
-  return <div></div>;
+import { ReactElement } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Home } from './components/Home';
+
+const App = (): ReactElement => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
