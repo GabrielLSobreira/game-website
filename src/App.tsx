4b9b474633e8d18d4c20d1104159d3164aff864b
Gabriel Sobreira <gabriellaranjasobreira@gmail.com>
2021-11-01 12:33:59 -0300

    Rotas Finalizadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { ReactElement } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Home } from './components/Home';
+import GlobalStyles from './components/GlobalStyles';
+import { GameInfo } from './components/GameInfo';
 
 const App = (): ReactElement => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Home} />
-      </Switch>
-    </BrowserRouter>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/game/:id" component={GameInfo} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 };
 
