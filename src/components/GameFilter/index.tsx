b616c33207e34d259fd692c7100f6e0808c40cf7
Gabriel Sobreira <gabriellaranjasobreira@gmail.com>
2021-11-01 12:37:12 -0300

    GameFilter Finalizado

--- src/components/GameFilter/index.tsx
+++ src/components/GameFilter/index.tsx
@@ -1,43 +1,44 @@
 import { ChangeEvent, ReactElement } from 'react';
 import { GENRES, PLATFORMS, SORT_BY } from './constants';
+import { Form, Label, Select } from './styles';
 
 interface Props {
   onChange: (e: ChangeEvent<HTMLFormElement>) => void;
 }
 
 export const GameFilter = ({ onChange }: Props): ReactElement => (
-  <form onChange={onChange}>
-    <label htmlFor="platform-select">
+  <Form onChange={onChange}>
+    <Label htmlFor="platform-select">
       Platform:
-      <select name="platform" id="platform-select">
+      <Select name="platform" id="platform-select">
         {PLATFORMS.map((platform) => (
           <option key={platform.value} value={platform.value}>
             {platform.display}
           </option>
         ))}
-      </select>
-    </label>
+      </Select>
+    </Label>
 
-    <label htmlFor="genre-select">
+    <Label htmlFor="genre-select">
       Genre:
-      <select name="genre" id="genre-select">
+      <Select name="genre" id="genre-select">
         {GENRES.map((genre) => (
           <option key={genre.value} value={genre.value}>
             {genre.display}
           </option>
         ))}
-      </select>
-    </label>
+      </Select>
+    </Label>
 
-    <label htmlFor="sortBy-select">
+    <Label htmlFor="sortBy-select">
       Sort By:
-      <select name="sortBy" id="sortBy-select">
+      <Select name="sortBy" id="sortBy-select">
         {SORT_BY.map((sortBy) => (
           <option key={sortBy.value} value={sortBy.value}>
             {sortBy.display}
           </option>
         ))}
-      </select>
-    </label>
-  </form>
+      </Select>
+    </Label>
+  </Form>
 );
